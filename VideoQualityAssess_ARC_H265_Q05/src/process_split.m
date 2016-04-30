1a49d03dd641cc6ad2ba95d1ee15141d424a7c0d
ZhangYong19800721 <24452861@qq.com>
2016-04-30 22:38:49 +0800

    fix a fatal bug of the code

--- VideoQualityAssess_ARC_H265_Q05/src/process_split.m
+++ VideoQualityAssess_ARC_H265_Q05/src/process_split.m
@@ -16,15 +16,15 @@ function process_split(splitno)
 	RecoverVideo_02000K = strcat(strcat('arc_', splitno),'_02000K.yuv');
 
 	[PSNR20000K PSNRf20000K PLLP20000K PLLPf20000K] = Video_FR_Q(SourceVideo,RecoverVideo_20000K,numOfFrame,frameWidth,frameHeight,'yuv420p');
-	[PSNR20000K PSNRf20000K PLLP18000K PLLPf18000K] = Video_FR_Q(SourceVideo,RecoverVideo_18000K,numOfFrame,frameWidth,frameHeight,'yuv420p');
-	[PSNR20000K PSNRf20000K PLLP16000K PLLPf16000K] = Video_FR_Q(SourceVideo,RecoverVideo_16000K,numOfFrame,frameWidth,frameHeight,'yuv420p');
-	[PSNR20000K PSNRf20000K PLLP14000K PLLPf14000K] = Video_FR_Q(SourceVideo,RecoverVideo_14000K,numOfFrame,frameWidth,frameHeight,'yuv420p');
-	[PSNR20000K PSNRf20000K PLLP12000K PLLPf12000K] = Video_FR_Q(SourceVideo,RecoverVideo_12000K,numOfFrame,frameWidth,frameHeight,'yuv420p');
-	[PSNR20000K PSNRf20000K PLLP10000K PLLPf10000K] = Video_FR_Q(SourceVideo,RecoverVideo_10000K,numOfFrame,frameWidth,frameHeight,'yuv420p');
-	[PSNR20000K PSNRf20000K PLLP08000K PLLPf08000K] = Video_FR_Q(SourceVideo,RecoverVideo_08000K,numOfFrame,frameWidth,frameHeight,'yuv420p');
-	[PSNR20000K PSNRf20000K PLLP06000K PLLPf06000K] = Video_FR_Q(SourceVideo,RecoverVideo_06000K,numOfFrame,frameWidth,frameHeight,'yuv420p');
-	[PSNR20000K PSNRf20000K PLLP04000K PLLPf04000K] = Video_FR_Q(SourceVideo,RecoverVideo_04000K,numOfFrame,frameWidth,frameHeight,'yuv420p');
-	[PSNR20000K PSNRf20000K PLLP02000K PLLPf02000K] = Video_FR_Q(SourceVideo,RecoverVideo_02000K,numOfFrame,frameWidth,frameHeight,'yuv420p');
+	[PSNR18000K PSNRf18000K PLLP18000K PLLPf18000K] = Video_FR_Q(SourceVideo,RecoverVideo_18000K,numOfFrame,frameWidth,frameHeight,'yuv420p');
+	[PSNR16000K PSNRf16000K PLLP16000K PLLPf16000K] = Video_FR_Q(SourceVideo,RecoverVideo_16000K,numOfFrame,frameWidth,frameHeight,'yuv420p');
+	[PSNR14000K PSNRf14000K PLLP14000K PLLPf14000K] = Video_FR_Q(SourceVideo,RecoverVideo_14000K,numOfFrame,frameWidth,frameHeight,'yuv420p');
+	[PSNR12000K PSNRf12000K PLLP12000K PLLPf12000K] = Video_FR_Q(SourceVideo,RecoverVideo_12000K,numOfFrame,frameWidth,frameHeight,'yuv420p');
+	[PSNR10000K PSNRf10000K PLLP10000K PLLPf10000K] = Video_FR_Q(SourceVideo,RecoverVideo_10000K,numOfFrame,frameWidth,frameHeight,'yuv420p');
+	[PSNR08000K PSNRf08000K PLLP08000K PLLPf08000K] = Video_FR_Q(SourceVideo,RecoverVideo_08000K,numOfFrame,frameWidth,frameHeight,'yuv420p');
+	[PSNR06000K PSNRf06000K PLLP06000K PLLPf06000K] = Video_FR_Q(SourceVideo,RecoverVideo_06000K,numOfFrame,frameWidth,frameHeight,'yuv420p');
+	[PSNR04000K PSNRf04000K PLLP04000K PLLPf04000K] = Video_FR_Q(SourceVideo,RecoverVideo_04000K,numOfFrame,frameWidth,frameHeight,'yuv420p');
+	[PSNR02000K PSNRf02000K PLLP02000K PLLPf02000K] = Video_FR_Q(SourceVideo,RecoverVideo_02000K,numOfFrame,frameWidth,frameHeight,'yuv420p');
 
 	save(strcat(splitno,'_result_arc.mat'));
 end
